3f5ea05970845d0e20c54d8f1e1379e233c03b88
thaophamhetxat <thaophamhetxat@gmail.com>
2022-05-17 23:57:14 +0700

    first commit

---
+++ src/main/java/com/example/demo/ShopKkSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopKkSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopKkSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/dto/ReportInventory.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReportInventory implements Serializable{
+	private Serializable group;
+	private Double sum;
+	private Long count;
+}
